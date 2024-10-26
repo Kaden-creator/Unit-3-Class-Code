cf62eae6c928edd4e6fb9467a13510696388dddb
Kaden-creator <meirk@wlhs.wlwv.k12.or.us>
2024-10-26 05:19:16 +0000

    done

---
+++ HW3.py
@@ -0,0 +1,41 @@
+age = int(input("How old is your cat in month? :"))
+if age <= 6:
+    print("Kittens cost $250")
+elif age <= 11:
+    print("Teenagers cost $225")
+elif age <= 95:
+    print("Adults cost $150")
+elif age >= 96:
+    print("Seniors cost $85")
+
+age = int(input("How old is your cat in month? :"))
+if age <= 6:
+    print("Kittens cost $250")
+if age <= 11:
+    print("Teenagers cost $225")
+if age <= 95:
+    print("Adults cost $150")
+if age >= 96:
+    print("Seniors cost $85")
+
+age = int(input("How old is your cat in month? :"))
+if age >= 96:
+    print("Seniors cost $85")
+if age <= 95:
+    print("Adults cost $150")
+if age <= 11:
+    print("Teenagers cost $225")
+if age <= 6:
+    print("Kittens cost $250")
+
+age = int(input("How old is your cat in month? :"))
+if age >= 96:
+    print("Seniors cost $85") 
+elif age <= 95:
+    print("Adults cost $150")
+elif age <= 11:
+    print("Teenagers cost $225")
+elif age <= 6:
+    print("Kittens cost $250")
+
+
